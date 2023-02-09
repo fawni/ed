9e9087775a80c29e130d8c34bbdd43928498af61
obscurity <z@x4.pm>
2023-02-09 22:10:21 +0200

    refactor: use command macro

--- src/args.rs
+++ src/args.rs
@@ -1,8 +1,7 @@
-use clap::Command;
+use clap::{command, Command};
 
 pub fn get_app() -> Command {
-    Command::new("ed")
-        .version(env!("CARGO_PKG_VERSION"))
+    command!()
         .subcommand(Command::new("bmi").about("Calculate BMI"))
         .subcommand(Command::new("bmr").about("Calculate BMR"))
         .subcommand(Command::new("tdee").about("Calculate TDEE"))
