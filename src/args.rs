13f25fb029fd9ee5edaada5820e95f9af5d23024
fawn <fawn@envs.net>
2023-03-14 18:32:43 +0200

    perf: one less iteration

--- src/args.rs
+++ src/args.rs
@@ -1,7 +1,7 @@
 use clap::{command, Command};
 
 pub fn get_app() -> Command {
-    command!()
+    command!("ed")
         .subcommand(Command::new("bmi").about("Calculate BMI"))
         .subcommand(Command::new("bmr").about("Calculate BMR"))
         .subcommand(Command::new("tdee").about("Calculate TDEE"))
